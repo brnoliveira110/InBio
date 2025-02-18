30b98b89054ed5115942d53312d7231debbbebc8
Caio Santana <brnoliveirasantana@gmail.com>
2025-02-17 22:22:44 -0300

    Criando seções de Video e Pricing

--- components/landing-page/hero.tsx
+++ components/landing-page/hero.tsx
@@ -6,8 +6,8 @@ import TextInput from "../ui/textInput";
 
 export default function Hero() {
   return (
-    <div className="flex ">
-      <div className="w-full flex flex-col gap-2 mt-[35vh] ">
+    <div className="flex  h-screen">
+      <div className="w-full flex flex-col gap-2 mt-[35vh]">
         <h1 className="text-5xl font-bold text-white leading-[64px]">
           Seus projetos e redes sociais em um único link
         </h1>
